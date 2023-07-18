2147851cea1d2a85c2b03d5f9abe55de6100d914
Naveh Afik <naveha@lightrun.com>
2023-07-18 19:15:40 +0300

    Text to morse app.

---
+++ text_to_morse.py
@@ -0,0 +1,36 @@
+import argparse
+
+# Morse Code Translator
+morse_code_dict = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
+def text_to_morse(text):
+    morse_code = ''
+    for char in text:
+        if char != ' ':
+            morse_code += morse_code_dict[char.upper()] + ' '
+        else:
+            morse_code += ' '
+    return morse_code
+
+
+parser = argparse.ArgumentParser(description='Convert text to Morse code.')
+parser.add_argument('text', type=str, help='The text to be converted to Morse code.')
+
+args = parser.parse_args()
+
+print(text_to_morse(args.text))
